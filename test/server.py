1a8f8bd9f5cf57252402bd3cea1410dadf62f0af
Gregory Petukhov <lorien@lorien.name>
2017-01-20 22:46:35 +0700

    Refactor tests to use pytest

--- test/server.py
+++ test/server.py
@@ -13,7 +13,6 @@ class ServerTestCase(TestCase):
     def setUpClass(cls):
         cls.server = TestServer()
         cls.server.start()
-        print('!!!')
 
     @classmethod
     def tearDownClass(cls):
