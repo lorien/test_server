ee0201406e44e231f3713f2ef5eca4449a805fe5
Gregory Petukhov <lorien@lorien.name>
2017-02-27 18:12:34 +0700

    Cleanup tests

--- test/server.py
+++ test/server.py
@@ -1,17 +1,3 @@
-"""
-Test server could be run in two modes: thread and subprocess.
-The later mode could be used when you doing something/anything with
-standard python socket library and do not want side-effects. Running test
-server in separate process ensures that test_server uses unmodified stdlib.
-
-Most tests functions in this module are duplicated with *_with_state
-function:
-* test_foo tests default test_server engine (based on thread) and operates
-    with request and response directly (in-place)
-* tests_foo_with_state tests subprocess engine (based on separate process) and
-    use settter/getter functions to access request and response because the
-    API and test server work in different processes.
-"""
 # pylint: disable=redefined-outer-name
 import time
 from threading import Thread
@@ -46,32 +32,32 @@ def skip_by_engine(request, opt_engine):
             pytest.skip('Skipped on engine %s' % opt_engine)
 
 
-def test_get_with_state(server):
+def test_get(server):
     valid_data = b'zorro'
     server.set_response('data', valid_data)
     data = urlopen(server.get_url()).read()
     assert data == valid_data
 
 
-def test_request_client_ip_with_state(server):
+def test_request_client_ip(server):
     urlopen(server.get_url()).read()
     assert server.address == server.get_request('client_ip')
 
 
-def test_path_with_state(server):
+def test_path(server):
     urlopen(server.get_url('/foo?bar=1')).read()
     assert server.get_request('path') == '/foo'
     assert server.get_request('args')['bar'] == '1'
 
 
-def test_post_with_state(server):
+def test_post(server):
     server.set_response('post.data', b'resp-data')
     data = urlopen(server.get_url(), b'req-data').read()
     assert data == b'resp-data'
     assert server.get_request('data') == b'req-data'
 
 
-def test_response_once_get_with_state(server):
+def test_response_once_get(server):
     server.set_response('data', b'base')
     assert urlopen(server.get_url()).read() == b'base'
     server.set_response_once('data', b'tmp')
@@ -79,7 +65,7 @@ def test_response_once_get_with_state(server):
     assert urlopen(server.get_url()).read() == b'base'
 
 
-def test_response_once_headers_with_state(server):
+def test_response_once_headers(server):
     server.set_response('headers', [('foo', 'bar')])
     info = urlopen(server.get_url())
     assert info.headers['foo'] == 'bar'
@@ -94,20 +80,20 @@ def test_response_once_headers_with_state(server):
     assert info.headers['foo'] == 'bar'
 
 
-def test_request_headers_with_state(server):
+def test_request_headers(server):
     req = Request(server.get_url(), headers={'Foo': 'Bar'})
     urlopen(req).read()
     assert server.get_request('headers')['foo'] == 'Bar'
 
 
-def test_response_once_reset_headers_with_state(server):
+def test_response_once_reset_headers(server):
     server.set_response_once('headers', [('foo', 'bar')])
     server.reset()
     info = urlopen(server.get_url())
     assert 'foo' not in info.headers
 
 
-def test_method_sleep_with_state(server):
+def test_method_sleep(server):
     delay = 0.3
 
     start = time.time()
@@ -122,7 +108,7 @@ def test_method_sleep_with_state(server):
     assert elapsed > delay
 
 
-def test_response_once_code_with_state(server):
+def test_response_once_code(server):
     info = urlopen(server.get_url())
     assert info.getcode() == 200
     server.set_response_once('code', 403)
@@ -132,13 +118,13 @@ def test_response_once_code_with_state(server):
     assert info.getcode() == 200
 
 
-def test_request_done_after_start_with_state(server):
+def test_request_done_after_start(server):
     server = TestServer()
     server.start()
     assert server.get_request('done') is False
 
 
-def test_request_done_with_state(server):
+def test_request_done(server):
     assert server.get_request('done') is False
     urlopen(server.get_url()).read()
     assert server.get_request('done') is True
@@ -158,7 +144,7 @@ def test_wait_timeout_error(server):
         server.wait_request(0.01)
 
 
-def test_response_once_cookies_with_state(server):
+def test_response_once_cookies(server):
     server.set_response('cookies', [('foo', 'bar')])
     info = urlopen(server.get_url())
     assert 'foo=bar' in info.headers['Set-Cookie']
@@ -178,7 +164,7 @@ def test_default_header_content_type(server):
     assert info.headers['content-type'] == 'text/html; charset=UTF-8'
 
 
-def test_custom_header_content_type_with_state(server):
+def test_custom_header_content_type(server):
     server.set_response('headers', [
         ('Content-Type', 'text/html; charset=koi8-r'),
     ])
@@ -192,7 +178,7 @@ def test_default_header_server(server):
             ('TestServer/%s' % test_server.__version__))
 
 
-def test_custom_header_server_with_state(server):
+def test_custom_header_server(server):
     server.set_response('headers', [
         ('Server', 'Google'),
     ])
@@ -200,7 +186,7 @@ def test_custom_header_server_with_state(server):
     assert info.headers['server'] == 'Google'
 
 
-def test_options_method_with_state(server):
+def test_options_method(server):
     server.set_response('data', b'abc')
 
     class RequestWithMethod(Request):
