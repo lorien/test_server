717b7edef76c85a79f37992d41b939d12968c45f
Gregory Petukhov <lorien@lorien.name>
2018-04-08 03:36:34 +0300

    Bump version: 0.0.27 → 0.0.28dev0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 setup(
     # Meta data
     name='test-server',
-    version='0.0.27',
+    version='0.0.28dev0',
     author='Gregory Petukhov',
     author_email='lorien@lorien.name',
     maintainer="Gregory Petukhov",
