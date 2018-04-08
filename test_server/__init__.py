d3e2c8cfb74d37ae71b03e18419e044aad60c1a9
Gregory Petukhov <lorien@lorien.name>
2018-04-08 03:41:14 +0300

    Bump version: 0.0.28dev0 → 0.0.28

--- test_server/__init__.py
+++ test_server/__init__.py
@@ -1,4 +1,4 @@
 from test_server.server import *  # noqa pylint: disable=wildcard-import
 from test_server.error import *  # noqa pylint: disable=wildcard-import
 
-__version__ = '0.0.28dev0'
+__version__ = '0.0.28'
