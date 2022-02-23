ab547cb855f51dacb662358d8885724723cb4178
Some User <guest@example.com>
2022-02-23 17:28:01 +0600

    Bump version: 0.0.34 → 0.0.35

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 setup(
     # Meta data
     name="test_server",
-    version="0.0.34",
+    version="0.0.35",
     author="Gregory Petukhov",
     author_email="lorien@lorien.name",
     maintainer="Gregory Petukhov",
