d2774bb40cf3ac8c63a4b1757ff7b2686f37f5f1
User <guest@example.com>
2023-08-01 13:47:51 +0600

    Add running tests for release version

---
+++ .github/workflows/test_release.py
@@ -0,0 +1,28 @@
+name: Tests
+
+on: ["push", "pull_request"]
+
+jobs:
+  test:
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        os: [ubuntu-latest, macos-latest, windows-latest]
+        python: ['3.8', '3.9', '3.10', '3.11', '3.12-dev']
+    steps:
+
+    - uses: actions/checkout@v2
+
+    - name: Set up Python ${{ matrix.python }}
+      uses: actions/setup-python@v4
+      with:
+        python-version: ${{ matrix.python }}
+
+    - name: Install dependencies
+      run: |
+        pip install -U -r requirements_dev.txt
+        pip install -U test_server
+
+    - name: Run tests
+      run: |
+        make pytest
