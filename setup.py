e8fe2bab871f8769f690e97997ab2253ed82ac44
User <guest@example.com>
2025-09-07 13:34:06 +0300

    Add setup.py script for python 2.7

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test_server",
+    version="0.0.43",
+    packages=["test_server"],
+    install_requires=[
+        "six",
+        'typing-extensions; python_version <= "2.7"',
+    ],
+)
